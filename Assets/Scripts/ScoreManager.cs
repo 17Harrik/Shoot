313c5bdc700aaf294b3e996adeab04e91cd75fc0
Luke Barkess <luke.barkess@icloud.com>
2022-02-03 15:30:33 +0000

    Added a simple score manager

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ScoreManager
+{
+    [HideInInspector]
+    public static int score;
+
+    public static void AddScore(int amount)
+    {
+        score += amount;
+    }
+
+    public static void RemoveScore(int amount)
+    {
+        score -= amount;
+    }
+}
